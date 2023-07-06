98f30ba226c006b2712e44729afd4c9eebb9c83b
Owen Ashurst <owen.ashurst@googlemail.com>
2023-07-06 19:29:34 +0100

    made static

--- SolarToTelegrafHTTPProxy/Services/Mqtt/Models/Config.cs
+++ SolarToTelegrafHTTPProxy/Services/Mqtt/Models/Config.cs
@@ -1,14 +1,6 @@
-﻿using System.Text.Json.Serialization;
-using SolarToTelegrafHTTPProxy.Config;
+﻿namespace SolarToTelegrafHTTPProxy.Services.Mqtt.Models;
 
-namespace SolarToTelegrafHTTPProxy.Services.Mqtt.Models;
-
-public class Config
+public static class Config
 {
     public const string StateTopicName = "homeassistant/sensor/iconica_solar/state";
-    
-    [JsonPropertyName("ac_output_apparent_power_t")]
-    public string ACOutputApparentPower { get; set; } = MqttSettings.MessageTopic;
-    [JsonPropertyName("ac_output_apparent_power_tpl")]
-    public string ACOutputApparentPowerTemplate { get; set; } = "{{ value_json.acOutputApparentPower }}";
 }
