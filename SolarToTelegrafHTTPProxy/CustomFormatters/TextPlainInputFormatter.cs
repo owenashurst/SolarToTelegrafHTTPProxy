8a411d6145a183c9d12be280c9a3944771250be9
Owen Ashurst <owen.ashurst@googlemail.com>
2023-07-06 13:33:37 +0100

    refactored

--- SolarToTelegrafHTTPProxy/CustomFormatters/TextPlainInputFormatter.cs
+++ SolarToTelegrafHTTPProxy/CustomFormatters/TextPlainInputFormatter.cs
@@ -16,17 +16,17 @@ namespace SolarToTelegrafHTTPProxy.CustomFormatters
         public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context)
         {
             var request = context.HttpContext.Request;
-            using (var reader = new StreamReader(request.Body))
-            {
-                var content = await reader.ReadToEndAsync();
-                return await InputFormatterResult.SuccessAsync(content);
-            }
+            
+            using var reader = new StreamReader(request.Body);
+            var content = await reader.ReadToEndAsync();
+            
+            return await InputFormatterResult.SuccessAsync(content);
         }
 
         public override bool CanRead(InputFormatterContext context)
         {
             var contentType = context.HttpContext.Request.ContentType;
-            return contentType.StartsWith(ContentType);
+            return contentType != null && contentType.StartsWith(ContentType);
         }
     }
 }
