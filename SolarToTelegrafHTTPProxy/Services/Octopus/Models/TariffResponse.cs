3bed283bdcdf4a46a63edaa132ec349c11ffbb72
Owen Ashurst <owen.ashurst@googlemail.com>
2023-10-22 15:31:56 +0100

    Update TariffResponse.cs

--- SolarToTelegrafHTTPProxy/Services/Octopus/Models/TariffResponse.cs
+++ SolarToTelegrafHTTPProxy/Services/Octopus/Models/TariffResponse.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace SolarToTelegrafHTTPProxy.Services.Octopus.Models;
 
 public class TariffResponse
 {
+    [JsonPropertyName("count")]
     public int Count { get; set; }
+    
+    [JsonPropertyName("next")]
     public string Next { get; set; }
+    
+    [JsonPropertyName("previous")]
     public string Previous { get; set; }
+    
+    [JsonPropertyName("results")]
     public IEnumerable<Tariff> Results { get; set; }
 }
