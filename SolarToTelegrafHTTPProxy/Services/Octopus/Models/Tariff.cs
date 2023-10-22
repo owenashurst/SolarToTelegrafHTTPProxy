58f81150fbc943e47b454360414d6302edfcf655
Owen Ashurst <owen.ashurst@googlemail.com>
2023-10-22 15:30:05 +0100

    added json property names to model properties

--- SolarToTelegrafHTTPProxy/Services/Octopus/Models/Tariff.cs
+++ SolarToTelegrafHTTPProxy/Services/Octopus/Models/Tariff.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Text.Json.Serialization;
 
 namespace SolarToTelegrafHTTPProxy.Services.Octopus.Models;
 
 public class Tariff
 {
+    [JsonPropertyName("value_exc_vat")]
     public double ValueExcVat { get; set; }
+    
+    [JsonPropertyName("value_inc_vat")]
     public double ValueIncVat { get; set; }
+    
+    [JsonPropertyName("valid_from")]
     public DateTime ValidFrom { get; set; }
+    
+    [JsonPropertyName("valid_to")]
     public DateTime ValidTo { get; set; }
-    public string PaymentMethod { get; set; }
 }
